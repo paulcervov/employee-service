3ee16b2fc862f1793b15704a8eaa0da2b97637e4
Paul Cervov <paulcervov@gmail.com>
2021-01-12 22:27:53 +0300

    Implement mutations for employees API

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,6 @@
 import {User} from "./entity/User";
 import {getRepository} from "typeorm";
 
-
 export const Query = {
     findUsers: (_, {first, offset}, context, info) => {
 
@@ -14,4 +13,47 @@ export const Query = {
     },
 }
 
+export const Mutation = {
+    createUser: async (_, {input}) => {
 
+        const user = getRepository(User).create(input);
+
+        await getRepository(User).save(user);
+
+        return {
+            success: true,
+            message: 'Сотрудник был создан',
+            user
+        }
+    },
+    updateUser: async (_, {id, input}) => {
+
+        const user = await getRepository(User).findOne(id);
+
+        await getRepository(User).update(id, input);
+
+        return {
+            success: true,
+            message: 'Сотрудник был обновлен',
+            user
+        }
+    },
+    deleteUser: async (_, {id}) => {
+
+        const user = await getRepository(User).findOne(id);
+
+        await getRepository(User).delete(id);
+
+        return {
+            success: true,
+            message: 'Сотрудник был удален',
+            user
+        }
+    },
+}
+
+export const MutationResponse = {
+    __resolveType() {
+        return null;
+    }
+}
--- src/schema.ts
+++ src/schema.ts
@@ -19,4 +19,39 @@ export const typeDefs = gql`
         position: String!
         role: Int!
     }
+
+    type Mutation {
+        # Storing new user
+        createUser(input: UserInput!): UserMutationResponse
+
+        # Update an existing user by id
+        updateUser(id: ID!, input: UserInput!): UserMutationResponse
+
+        # Delete an existing user by id
+        deleteUser(id: ID!): UserMutationResponse
+
+        # Restore an existing user by id
+        restoreUser(id: ID!): UserMutationResponse
+    }
+
+    input UserInput {
+        firstName: String!
+        middleName: String!
+        lastName: String!
+        dateOfBirth: String!
+        address: String!
+        position: String!
+        role: Int!
+    }
+
+    interface MutationResponse {
+        success: Boolean!
+        message: String!
+    }
+
+    type UserMutationResponse implements MutationResponse {
+        success: Boolean!
+        message: String!
+        user: User!
+    }
 `;
