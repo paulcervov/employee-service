f64228fc4a353e8c2a0f58990ccbd7c059956e8b
Paul Cervov <paulcervov@gmail.com>
2021-01-13 16:27:28 +0300

    Add phone and password field to user entity and seeder.ts

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 import {Role} from '../enums/Role'
 
 @Entity()
@@ -8,13 +8,19 @@ export class User {
     id: number;
 
     @Column()
+    phone: string;
+
+    @Column()
+    password: string;
+
+    @Column()
     firstName: string;
 
     @Column()
-    lastName: string;
+    middleName: string;
 
     @Column()
-    middleName: string;
+    lastName: string;
 
     @Column('date')
     dateOfBirth: string;
--- src/seeder.ts
+++ src/seeder.ts
@@ -1,32 +1,66 @@
 import * as faker from 'faker/locale/ru';
-import {createConnection, getRepository} from "typeorm";
+import {createConnection, getRepository, Repository} from "typeorm";
 import {User} from "./entity/User";
 import {Role} from './enums/Role';
 
 createConnection().then(async connection => {
 
-    const userRepository = getRepository(User);
+    console.log("Database seeding...");
+
+    const userRepository: Repository<User> = getRepository(User);
     const users: User[] = [];
-    const roles = Object.values(Role).filter(v => typeof v === 'number');
+    const roles: number[] = [];
+
+    Object.values(Role).forEach((v) => {
+        if (typeof v === 'number') {
+            roles.push(v);
+        }
+    });
 
     for (let i = 1; i < 30; i++) {
 
-        const gender: number[] = faker.random.arrayElement([1, 2]);
+        const gender: number = faker.random.arrayElement([0, 1]);
 
         const user: User = userRepository.create({
+
+            phone: `+${faker.random.number({min: 1, max: 9})}${faker.unique(faker.phone.phoneNumber)}`,
+            password: '$2b$04$toP1b1dhS0dtpPicQsWjjeOwzl3pvPAfgHDHMT.FXUDy2fuCd4u86', // password,
+
             lastName: faker.name.lastName(gender),
             firstName: faker.name.firstName(gender),
             middleName: faker.name.middleName(gender),
-            dateOfBirth: faker.date.between('1950', '2010')
-                .toISOString().split('T').shift(),
+
+            dateOfBirth: faker.date.between('1950', '2010').toISOString().split('T').shift(),
             address: `${faker.address.state()}, ${faker.address.city()}, ${faker.address.streetAddress()}, ${faker.address.secondaryAddress()}`,
             position: faker.name.jobTitle(),
+
             role: faker.random.arrayElement(roles)
         });
 
         users.push(user);
     }
 
+    // hardcode for demo access
+    userRepository.merge(users[0], {
+        phone: '+1(111)111-11-1',
+        position: 'Сотрудник',
+        role: Role.Employee
+    });
+
+    userRepository.merge(users[1], {
+        phone: '+2(222)222-22-22',
+        position: 'Кадровик',
+        role: Role.HR
+    });
+
+    userRepository.merge(users[2], {
+        phone: '+3(333)333-33-33',
+        position: 'Директор',
+        role: Role.Director
+    });
+
     await userRepository.save(users);
+
+    console.log("Database seeding completed successfully");
 
 }).catch(error => console.log(error));
